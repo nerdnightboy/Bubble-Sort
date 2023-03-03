844698afff7f47e7fc650b500b69c105f9699626
nerdnightboy <nerdnightboy@gmail.com>
2023-03-03 17:13:26 +0530

    Flag Added

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,11 +7,14 @@ void bubbleSort(vector<int> &v){
 
     for (int i = 0; i < n-1; i++)
     {
+        bool flag = false;
         for (int j = 0; j < n-i-1; j++)
         {
             if(v[j]>v[j+1]){
+                flag = true;
                 swap(v[j], v[j+1]);
             }
+            if(!flag) break;
         }
         
     }
